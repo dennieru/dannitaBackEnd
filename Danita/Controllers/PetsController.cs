f87d3d3ed3184019b06f1335e7e7cd2b784b7e24
JALASOFT\Daniel Pacheco <Daniel.Pacheco@jalasoft.com>
2017-05-11 12:36:00 -0400

    Adding middleware layer

--- Danita/Controllers/PetsController.cs
+++ Danita/Controllers/PetsController.cs
@@ -1,27 +1,49 @@
 ﻿using System;
 using System.Web.Http;
 using System.Collections.Generic;
+using Danita.Models;
+using System.Linq;
+using Danita.Adapters;
 
 namespace Danita
 {
 	public class PetsController : ApiController
 	{
-		// Get api/pets
-		public IEnumerable<string> Get ()
+        public PetsController()
+        {
+        }
+
+        // Get api/pets
+        [Route("pets")]
+        public IEnumerable<Pet> Get ()
 		{
-            int version = 5;
-			return new string[] { $"Colitas{version}", "Tomboy"};
+            return MainAdapter.Instance.GetList<Pet>();
 		}
 
 		// Get api/pets/{id}
-		public string Get (int id)
-		{
-			return "Colitas";
-		}
+		public Pet Get (string id)
+        {
+            Pet pet = new Pet();
+            pet = MainAdapter.Instance.GetList<Pet>().FirstOrDefault(x => x.Id == new Guid(id));
+            pet.Sons = MainAdapter.Instance.GetGuids<Owner>().ToList();
 
-		// POST api/pets
-		public void Post([FromBody]string value)
+            return pet;
+        }
+
+        // POST api/pets
+        [Route("api/pets")]
+        public string Post([FromBody]Pet pet)
 		{
+            Guid guid = pet.Id;
+            if (pet.Id == null || pet.Id == Guid.Empty)
+            {
+                guid = Guid.NewGuid();
+                pet.Id = guid;
+            }
+            pet.Sons = MainAdapter.Instance.GetGuids<Owner>().ToList();
+            Pet petAdded = MainAdapter.Instance.AddItem<Pet>(pet);
+
+            return petAdded.Id.ToString();
 		}
 
 		// PUT api/pets/{id}
@@ -29,9 +51,12 @@ namespace Danita
 		{
 		}
 		// DELETE api/pets/{id}
-		public void Delete(int id)
+		public bool Delete(string id)
 		{
-		}
+            Pet pet = MainAdapter.Instance.GetList<Pet>().FirstOrDefault(x => x.Id == new Guid(id));
+
+            return MainAdapter.Instance.RemoveItem(pet);
+        }
 	}
 }
 
---
+++ Danita/Models/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Danita.Models
+{
+    public class Entity
+    {
+        public List<Guid> Sons { get; set; }
+    }
+}
